15a538fea96a976f892d3d3f056be7aac0cd5691
Matthew Scharley <matt@scharley.me>
2024-06-17 23:34:52 +1000

    chore: whitelist all passing tests [651/1193]

--- bin/update-blacklist.sh
+++ bin/update-blacklist.sh
@@ -2,4 +2,4 @@
 
 # Intended to be used as `./bin/update-blacklist.sh >> test/commonmark_test/spec.gleam` and then
 # manually cut and paste the results into the blacklist.
-gleam test | grep FAIL | sed -E 's/.* Example ([[:digit:]]+)/\1/' | sort -n | sed -e 's/$/,/'
+gleam test | grep FAIL | grep -v '(strict)' | sed -E 's/.* Example ([[:digit:]]+)/\1/' | sort -n | sed -e 's/$/,/'
