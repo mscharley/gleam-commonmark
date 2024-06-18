851c707b66a984c65bab5ade730767294d3cf25a
Matthew Scharley <matt@scharley.me>
2024-06-18 20:58:26 +1000

    chore: publish unbundled code to the demo for testing

--- bin/build-demo.sh
+++ bin/build-demo.sh
@@ -8,7 +8,7 @@ set -x
 gleam run -m lustre/dev build
 
 # Move all the static files into place
-for f in index.html build/dev/javascript/lustre_ui/priv priv; do
+for f in index.html build/dev/javascript/prelude.mjs build/dev/javascript/gleam_stdlib build/dev/javascript/commonmark build/dev/javascript/lustre_ui/priv priv; do
 	mkdir -p ./out/$(dirname $f)
 	cp -r ./$f ./out/$f
 done
